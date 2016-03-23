0ab44dc14b0afeac451ac90548e1e44684f74c6b
Alfonso Kim <adkim.ext@gmv.com>
2016-03-23 11:21:35 -0600

    cambios a la funcion que devuelve los layers, no funciona para las leyendas

--- scalability_study/scalability.R
+++ scalability_study/scalability.R
@@ -12,73 +12,27 @@ one_billion$conts = one_billion$cont1 + one_billion$cont2
 
 head(one_billion, 10)
 
-scalability_plot(one_billion, measure_var="total", value_var="locs", group_var="size") +
+
+?geom_point
+scale_colour_manual(name="Execution time", 
+                    values=c("black"="black", "blue"="blue"),
+                    labels = c('Total','Step 1')) + 
+
+steps <- c("Total", "Step 0", "Step 1", "Step 2", "Step 3", "Step 4",
+           "Step 5", "Step 6", "Step 7", "Step 8", "Step 9")
+dummy <- seq(1, 11)
+labels <- data.frame(steps, x=dummy, y=dummy)
+
+ggplot(labels, aes(x=x, y=y, colour=steps)) + 
+  scalability_plot(one_billion, measure_var="total", value_var="locs", group_var="size", colour="black") +
+  scalability_plot(one_billion, measure_var="step1", value_var="locs", group_var="size", colour="blue") +
+  theme_bw() +
   scale_y_continuous(breaks = seq(0, 700, 50)) +
-  ggtitle(expression(atop("Strong Scalability (total)", atop("One billion elements", "")))) +
+  ggtitle(expression(atop("Scalability", atop("20 Gigabytes", "")))) +
   xlab("Locations") +
   ylab("Time (seconds)")
 
 
-scalability_plot(one_billion, measure_var="step1", value_var="locs", group_var="size") +
-  scale_y_continuous(breaks = seq(0, 100, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 1)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-scalability_plot(one_billion, measure_var="step2", value_var="locs", group_var="size") +
-  scale_y_continuous(breaks = seq(0, 100, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 2)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
 
 
-scalability_plot(one_billion, measure_var="step3", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 100, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 3)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
 
-scalability_plot(one_billion, measure_var="step4", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 150, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 4)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-scalability_plot(one_billion, measure_var="step5", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 50, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 5)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-scalability_plot(one_billion, measure_var="step6", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 100, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 6)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-scalability_plot(one_billion, measure_var="step7", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 80, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 7)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-
-scalability_plot(one_billion, measure_var="step8", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 100, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 8)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-
-scalability_plot(one_billion, measure_var="step0", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 50, 5)) +
-  ggtitle(expression(atop("Strong Scalability (step 0)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
-
-
-scalability_plot(one_billion, measure_var="step9", value_var="locs", group_var="size") + 
-  scale_y_continuous(breaks = seq(0, 50, 10)) +
-  ggtitle(expression(atop("Strong Scalability (step 9)", atop("One billion elements", "")))) +
-  xlab("Locations") +
-  ylab("Time (seconds)")
--- scalability_study/scalability_plot.R
+++ scalability_study/scalability_plot.R
@@ -1,7 +1,7 @@
 
 source("c:\\Users\\Alfonso\\workspace\\thesis_scripts\\scalability_study\\summary.R")
 
-scalability_plot <- function(data, measure_var, value_var, group_var) {
+scalability_plot <- function(data, measure_var, value_var, group_var, colour="blue", size=0.5) {
   
   library("ggplot2")
   
@@ -12,13 +12,25 @@ scalability_plot <- function(data, measure_var, value_var, group_var) {
   data_se$y_min = data_se[,measure_var] - data_se$ci
   data_se$y_max = data_se[,measure_var] + data_se$ci
   
-  s_plot <- ggplot(data_se, aes_string(x=value_var, y=measure_var, group=group_var),
-                   environment = environment()) + 
-    geom_errorbar(aes(ymin=y_min, ymax=y_max),
-                  colour="black", width=0.2, position=position_dodge(0.1),
-                  environment = environment()) +
-    geom_line(position=position_dodge(0.1), size=0.5, colour="blue") +
-    geom_point(position=position_dodge(0.1), size=3)
+  layers <- list(
+    geom_line(
+      aes_string(x=value_var, y=measure_var, ymax=max(measure_var), group=group_var),
+      data=data_se, position=position_dodge(0.1), size=size, colour=colour, 
+      show_guide=TRUE,
+      environment = environment()
+    ),
+    geom_errorbar(
+      aes_string(x=value_var, y=measure_var, group=group_var, ymin="y_min", ymax="y_max"),
+      data=data_se, colour="black", width=0.2, position=position_dodge(0.1),
+      environment = environment()
+    ),
+    geom_point(
+      aes_string(x=value_var, y=measure_var, ymax=max(measure_var), group=group_var),
+      data=data_se, position=position_dodge(0.1), size=3,
+      colour=colour, show_guide=TRUE,
+      environment = environment()
+    )
+  )
   
-  return(s_plot)
+  return(layers)
 }
