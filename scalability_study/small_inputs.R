160f132ed18fda5286b39a088ecf4bc31b5745cd
Alfonso Kim <adkim.ext@gmv.com>
2016-03-24 15:46:28 -0600

    grafica de multiples entradas

--- scalability_study/small_inputs.R
+++ scalability_study/small_inputs.R
@@ -17,22 +17,22 @@ head(total)
 total.a <- melt(total, id.vars=c("locs", "size"), value.name="Time", 
                 variable.name="Total", measure.vars=c("total"))
 
-ggplot(data=total.a, aes(x=locs, y=total)) + 
-  geom_bar(aes(x=locs, y=Time),
-           position="stack", stat="identity") + 
-  geom_line(data=total, aes(x=locs, y=total), 
-            position=position_dodge(0.1), size=1, color="darkblue") +
-  # geom_point(aes(x=locs, y=total), 
-  #            position=position_dodge(0.1), size=3) +
-  # geom_errorbar(aes(x=locs, y=total, ymin=total-ci, ymax=total+ci),
-  #               data=total.a, colour="black", width=0.2, 
-  #               position=position_dodge(0.1)) +
-  # scale_y_continuous(breaks=seq(0, 700, 50)) +
-  facet_wrap(~size, nrow=5, ncol=2) + 
+
+## mydf$task <- factor(mydf$task, levels = c("up", "down", "left", "right", "front", "back"))
+
+total.a$labels <- factor(total.a$locs,
+                         levels=levels(total.a$locs), 
+                         labels=paste("Num cores: ", levels(total.a$locs), sep=""))
+ ### Esta ya funciona
+ggplot(total.a) + 
+  geom_point(aes(x=size, y=Time), size=3) + 
+  geom_line(aes(x=size, y=Time, group=1), 
+            colour="darkblue", position=position_dodge(0.1), size=1) + 
+  facet_wrap(~labels, scales="free", nrow=5, ncol=2) + 
   theme_bw() +
-  # scale_fill_brewer(palette="Paired", breaks=vars, labels=labels) + 
-  ggtitle(expression(atop("Strong Scalability", atop("1 billion elements", "")))) +
-  xlab("Cores (Locations)") +
-  ylab("Time (seconds)")
+  ggtitle(expression(atop("Strong Scalability", atop("Multiple Sizes", "")))) +
+  xlab("Input size") +
+  ylab("Time (seconds)") +
+  theme(axis.title=element_text(size=14))
 
 
